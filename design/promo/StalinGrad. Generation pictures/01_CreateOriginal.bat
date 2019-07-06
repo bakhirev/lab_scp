f86534b3dc4cd6a180794df2080f37c202d6d09d
alexey-bakhirev@yandex.ru <alexey-bakhirev@yandex.ru>
2019-07-06 13:36:52 +0400

    SCP v.0.0.4 Remove design

--- design/promo/StalinGrad. Generation pictures/01_CreateOriginal.bat
+++
@@ -1,8 +0,0 @@
-copy big.png middle.png
-copy big.png small.png
-
-copy big_iPhone.png middle_iPhone.png
-copy big_iPhone.png small_iPhone.png
-
-copy big.png promo_1400x560.png
-convert big.png -resize 800x800 -gravity center -crop 480x800+0+0 splashscreen_480x800.png
